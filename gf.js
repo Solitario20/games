6711e23b8106a04990238ef19f7da08490b1cbcc
cabir <cabir@gmail.com>
2024-12-04 04:43:18 -0800

    first commit

---
+++ gf.js
@@ -0,0 +1,155 @@
+function fp1() {
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `14`);
+    }, 1000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `13`);
+    }, 2000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `12`);
+    }, 3000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `11`);
+    }, 4000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `10`);
+    },5000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `9`);
+    }, 6000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `8`);
+    }, 7000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `7`);
+    }, 8000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `6`);
+    }, 9000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `5`);
+    }, 10000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `4`);
+    }, 11000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `3`);
+    }, 12000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `2`);
+    }, 13000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `1`);
+    }, 14000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice2").innerHTML =  `Neftçi PFK`);
+    }, 15000);
+    
+    }
+    
+    fp1()
+    function fp2() {
+    
+    
+    
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `14`);
+    }, 19000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `13`);
+    }, 20000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `12`);
+    }, 21000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `11`);
+    }, 20500);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `10`);
+    },22000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `9`);
+    }, 23000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `8`);
+    }, 24000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `7`);
+    }, 25000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `6`);
+    }, 26000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `5`);
+    }, 27000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `4`);
+    }, 28000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `3`);
+    }, 29000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `2`);
+    }, 30000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `1`);
+    }, 31000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice3").innerHTML =  `Araz Naxçıvan PFK`);
+    }, 32000);
+    
+    }
+    
+    fp2()
+    function fp() {
+    
+    
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `14`);
+    }, 35000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `13`);
+    }, 36000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `12`);
+    }, 37000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `11`);
+    }, 38000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `10`);
+    },39000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `9`);
+    }, 40000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `8`);
+    }, 41000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `7`);
+    }, 42000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `6`);
+    }, 43000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `5`);
+    }, 44000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `4`);
+    }, 45000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `3`);
+    }, 46000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `2`);
+    }, 47000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `1`);
+    }, 48000);
+    setTimeout(() => {
+        console.log(document.getElementById("netice1").innerHTML =  `Real Madrid`);
+    }, 49000);
+    
+    }
+    
+    fp()
